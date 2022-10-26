ed7cdacf4c8bb66f2d15e1f4f05126a49068fd79
Ulrik Røsby <ulrik@rosby.no>
2022-10-26 19:13:48 +0200

    feat: add price to ProductItem

--- vegfind-app/src/components/ProductItem.tsx
+++ vegfind-app/src/components/ProductItem.tsx
@@ -1,10 +1,16 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { useMemo } from "react";
 import { ProductProjected } from "../helpers/types";
 
 type ProductItemProps = {
   product: ProductProjected;
 };
 export default function ProductItem(props: ProductItemProps) {
+  const shops = props.product.shopsWithProduct;
+  const lowestPrice = useMemo(
+    () => (shops !== undefined ? Math.min(...shops.map((item) => item.price)) : undefined),
+    [props],
+  );
   return (
     <Card>
       <CardMedia component="img" alt="" height="150" image={props.product.imageUrl} />
@@ -13,6 +19,7 @@ export default function ProductItem(props: ProductItemProps) {
         <Typography>{props.product.brand}</Typography>
         <Typography>{props.product.type}</Typography>
         <Typography>{props.product.categories?.join(", ")}</Typography>
+        {lowestPrice && <Typography>From {lowestPrice.toFixed(2)} kr</Typography>}
       </CardContent>
     </Card>
   );
