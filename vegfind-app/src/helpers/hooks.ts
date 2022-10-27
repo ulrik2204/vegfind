84b02495fd4ec90f96b1285c238a47f448d21fe3
Ulrik Røsby <ulrik@rosby.no>
2022-10-27 13:11:28 +0200

    feat: add useDebouce hook

--- vegfind-app/src/helpers/hooks.ts
+++ vegfind-app/src/helpers/hooks.ts
@@ -1,1 +1,18 @@
-export const b = 4;
+import { useEffect, useState } from "react";
+
+export default function useDebounce(value: string, delay = 500) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler: NodeJS.Timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cancel the timeout if value changes (also on delay change or unmount)
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
